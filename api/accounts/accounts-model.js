5ca70f165aed0feb5370e95d633682f23d02f527
Yongwoo Lee <yw6636@gmail.com>
2023-07-01 12:09:22 -0400

    Finished all of the tasks given

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,36 @@
+const db = require('../../data/db-config')
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+  // select * from accounts;
+  return db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  // select * from accounts where id = 1;
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  // insert into accounts (name, budget) values ('foo', 1000);
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  // update accounts set name='foo', budget=1000 where id=1;
+  await db('accounts').where('id', id).update(account)
+  return getById(id)
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  // delete from accounts where id = 1;
+  return db('accounts').where('id', id).del()
 }
 
 module.exports = {
